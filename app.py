21ce41997bbe0dfbfc92da51608254f0de2e2045
Mohammad Shakib <shakibur2004@gmail.com>
2026-05-12 22:20:24 +0800

    Added new profile page

--- app.py
+++ app.py
@@ -1092,8 +1092,5 @@ def live_search():
 
 # RUN APP
 
-import os
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
